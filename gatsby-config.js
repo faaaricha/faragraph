e22eb15a27c264c64b9dabe4fc5716814051fcc5
faaaricha <afmantika@gmail.com>
2022-12-05 14:14:47 +0700

    New

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://headlessmultilingual.gatsbyjs.io',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-styled-components',
+      options: {
+        fileName: isDev,
+        displayName: isDev,
+        pure: true,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
+    },
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: '6188cf1d418f9084f1a921b2f935e2',
+        localeFallbacks: {
+          'ar-AE': 'en',
+        },
+      },
+    },
+  ],
+};
